ee1407bd7c33f048c6b3825e4f83a3bdf3f35ab1
Vitor Fonseca <vitor.fonseca@amplitarget.com.br>
2019-02-13 14:22:36 -0200

    now we are collecting "carro"

--- app/collect/controllers.py
+++ app/collect/controllers.py
@@ -7,12 +7,14 @@ import logging
 import os, string, re, sys, decimal
 import mysql.connector
 import requests
+import time
+
 
 # MODULE COLLECT
 collect = Blueprint("collect", __name__)
 
 #
-# Creates a coefficient table
+# Collect "marca"
 #
 @collect.route("/marca", methods=["GET"])
 def marca():
@@ -49,6 +51,95 @@ def marca():
     except Exception as e:  
         # log
         logging.error("An exception happened", exc_info=True)
+
+        # Return data
+        returnData = json.dumps({ "data": None, "message": "Something went wrong!" })
+        return Response(returnData, status=500, mimetype="application/json")
+
+#
+# Collect "carro"
+#
+@collect.route("/carro", methods=["GET"])
+def carro():
+    try: 
+        # MySQL Connection
+        mydb = mysql.connector.connect(
+            host=os.environ["MYSQL_HOST"],
+            user=os.environ["MYSQL_USER"],
+            passwd=os.environ["MYSQL_PASSWORD"],
+            database=os.environ["MYSQL_DATABASE"],
+        )
+
+        # Variable to store collected data
+        collectedMarcas = []
+
+        # Get stored "marca" in database to search "carro" of each one
+        cursosSelectMarca = mydb.cursor()
+        cursosSelectMarca.execute("SELECT id, nome FROM api_marca WHERE status = 0")
+        myresult = cursosSelectMarca.fetchall()
+        
+        # Interate each "marca" to get its "carros"
+        for marca in myresult:
+            marcaId = marca[0]
+            marcaName = marca[1]
+
+            # Get data
+            r = requests.get("http://fipeapi.appspot.com/api/1/carros/veiculos/{}.json".format(marcaId))
+            carros = r.json()
+
+            # Check if the request limit has been exceeded
+            if (r.status_code == 403):
+                print("--- REQUESTS LIMIT EXCEEDED ---")
+
+                # sleep 1 minute to start again
+                time.sleep( 61 )
+                print("--- I'M BACK ---")
+
+                # Try again
+                r = requests.get("http://fipeapi.appspot.com/api/1/carros/veiculos/{}.json".format(marcaId))
+                carros = r.json()
+
+
+            # Sql insert
+            sqlInsert = "INSERT INTO api_carro (id, nome, marca_id, status) VALUES (%s, %s, %s, %s)"
+            sqlInsertItems = []
+
+            # Iterate returned data to add "carro" to sql insert
+            for carro in carros:
+                sqlInsertItems.append((carro["id"], carro["fipe_name"], marcaId, 0))
+
+            # Execute insert
+            cursosInsertCarro = mydb.cursor()
+            cursosInsertCarro.executemany(sqlInsert, sqlInsertItems)
+
+            if (cursosInsertCarro.rowcount > 0):
+                # Append to collected "maca"
+                collectedMarcas.append(marcaName)
+
+                # Save status
+                cursosUpdateMarca = mydb.cursor()
+                cursosUpdateMarca.execute("UPDATE api_marca SET `status` = 1 WHERE id = {}".format(marcaId))
+                mydb.commit()
+
+        # Return data
+        returnData = json.dumps({"message": "Collected \"marca\" at data property", "data": collectedMarcas})
+        return Response(returnData, status=200, mimetype="application/json")
+    except Exception as e:  
+        # Log
+        logging.error("An exception happened", exc_info=True)
+
+        # MySQL Connection
+        mydb = mysql.connector.connect(
+            host=os.environ["MYSQL_HOST"],
+            user=os.environ["MYSQL_USER"],
+            passwd=os.environ["MYSQL_PASSWORD"],
+            database=os.environ["MYSQL_DATABASE"],
+        )
+
+        # Delete uncompleted "marca"
+        cursosDeleteUncompletedMarca = mydb.cursor()
+        cursosDeleteUncompletedMarca.execute("DELETE FROM api_carro WHERE marca_id IN (SELECT id FROM api_marca WHERE status = 0)")
+        mydb.commit()
 
         # Return data
         returnData = json.dumps({ "data": None, "message": "Something went wrong!" })
